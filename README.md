9b47203b3e4499a3dd84ec6b275e37e20d730844
lahgzz <lahg87@gmail.com>
2018-10-02 09:56:16 -0300

    up

---
+++ README.md
@@ -0,0 +1,6 @@
+# risk_utils
+Librería de utilería para riesgos data
+
+## instalación
+- Instalación local: pip install <PATH>/risk
+- Instalación symlink: pip install -e <PATH>/risk
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(name='risk_utils',
       version='1.0',
-      description=u"Librería de utilería de riesgos",
+      description=u"Librería de utilería para riesgos data",
       author=u'Luis Hernández | Data - San Jorge',
       author_email='lhernandez@amsanjorge.com.ar',
       packages=['risk_utils'],
